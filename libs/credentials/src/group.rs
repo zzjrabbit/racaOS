b44013b6f0c5312a2288ae7823cf350e4a347630
zzjrabbit <239873059@qq.com>
2025-10-14 22:36:37 +0800

    add gid & uid

--- libs/credentials/src/group.rs
+++ libs/credentials/src/group.rs
@@ -5,15 +5,15 @@ pub struct Gid(u32);
 impl Gid {
     pub const INVALID: Self = Self(u32::MAX);
     pub const OVERFLOW: Self = Self(65534);
-    
+
     pub const fn new(gid: u32) -> Self {
         Self(gid)
     }
-    
+
     pub const fn new_root() -> Self {
         Self(0)
     }
-    
+
     pub const fn is_root(&self) -> bool {
         self.0 == Self::new_root().0
     }
--- libs/credentials/src/user.rs
+++ libs/credentials/src/user.rs
@@ -5,15 +5,15 @@ pub struct Uid(u32);
 impl Uid {
     pub const INVALID: Self = Self(u32::MAX);
     pub const OVERFLOW: Self = Self(65534);
-    
+
     pub const fn new(uid: u32) -> Self {
         Self(uid)
     }
-    
+
     pub const fn new_root() -> Self {
         Self(0)
     }
-    
+
     pub const fn is_root(&self) -> bool {
         self.0 == Self::new_root().0
     }
