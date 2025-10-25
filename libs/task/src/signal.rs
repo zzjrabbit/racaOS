dde3f43f044d86a1706fcbd3c3876d320ac51853
zzjrabbit <239873059@qq.com>
2025-10-25 16:49:29 +0800

    add signal stack

--- libs/task/src/signal.rs
+++ libs/task/src/signal.rs
@@ -4,6 +4,7 @@ mod event;
 mod mask;
 mod queues;
 mod signal;
+mod stack;
 
 pub use action::*;
 pub use disposition::*;
@@ -11,3 +12,4 @@ pub use event::*;
 pub use mask::*;
 pub use queues::*;
 pub use signal::*;
+pub use stack::*;
---
+++ libs/task/src/signal/stack.rs
@@ -0,0 +1,103 @@
+use bitflags::bitflags;
+use ostd::mm::Vaddr;
+
+#[derive(Debug, Default)]
+pub struct SigStack {
+    base: Vaddr,
+    flags: SigStackFlags,
+    size: usize,
+}
+
+#[derive(Debug, PartialEq, Eq)]
+pub enum SigStackStatus {
+    /// The stack is enabled but currently inactive.
+    Inactive,
+    /// The stack is currently active.
+    Active,
+    /// The stack is disabled.
+    Disable,
+}
+
+bitflags! {
+    #[derive(Debug, Default, Clone, Copy, PartialEq, Eq)]
+    pub struct SigStackFlags: u32 {
+        const SS_ONSTACK = 1 << 0;
+        const SS_DISABLE = 1 << 1;
+        const SS_AUTODISARM = 1 << 31;
+    }
+}
+
+impl SigStack {
+    /// Creates a new signal stack.
+    pub fn new(base: Vaddr, flags: SigStackFlags, size: usize) -> Self {
+        Self { base, flags, size }
+    }
+
+    /// Returns the lowest address of the signal stack.
+    pub fn base(&self) -> Vaddr {
+        self.base
+    }
+
+    /// Returns the signal stack flags as set by the user.
+    pub fn flags(&self) -> SigStackFlags {
+        self.flags
+    }
+
+    /// Returns the current active status of the signal stack
+    /// based on the given stack pointer.
+    pub fn active_status(&self, sp: usize) -> SigStackStatus {
+        if self.size == 0 {
+            return SigStackStatus::Disable;
+        }
+
+        if self.contains(sp) {
+            return SigStackStatus::Active;
+        }
+
+        SigStackStatus::Inactive
+    }
+
+    /// Returns the signal stack size.
+    pub fn size(&self) -> usize {
+        self.size
+    }
+
+    /// Returns whether the given stack pointer is currently on the alternate signal stack.
+    ///
+    /// Note that if the `SS_AUTODISARM` flag is set,
+    /// the alternate signal stack is automatically disarmed after use.
+    /// In this case, even if `sp` lies within the stack range,
+    /// we consider that the signal stack is not active.
+    pub fn contains(&self, sp: usize) -> bool {
+        if self.flags().contains(SigStackFlags::SS_AUTODISARM) {
+            return false;
+        }
+
+        // The stack grows down, so `self.base` is exclusive.
+        self.base < sp && sp <= self.base + self.size
+    }
+
+    /// Resets the signal stack settings.
+    pub(super) fn reset(&mut self) {
+        self.base = 0;
+        self.size = 0;
+        self.flags = SigStackFlags::SS_DISABLE;
+    }
+}
+
+#[allow(non_camel_case_types)]
+pub struct stack_t {
+    pub ss_sp: Vaddr,
+    pub ss_flags: u32,
+    pub ss_size: usize,
+}
+
+impl From<&SigStack> for stack_t {
+    fn from(value: &SigStack) -> Self {
+        Self {
+            ss_sp: value.base,
+            ss_flags: value.flags.bits(),
+            ss_size: value.size,
+        }
+    }
+}
